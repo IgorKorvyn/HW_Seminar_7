2404a62a8544c0221046cc5a3a45a50d81841e68
Korvyn <korvyn555@google.com>
2022-10-27 19:28:12 +0300

    50

--- 52/Program.cs
+++ 52/Program.cs
@@ -6,54 +6,50 @@
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
 
-int[,] InputMatrix(int rows, int columns, int leftRange = 0, int rightRange = 9)
-{
-    int[,] matrix = new int[rows, columns];
-    Random rand = new Random();
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
-        }
-    }
-    return matrix;
-}
-
-void PrintMatrix(int[,] matrix)
+void InputMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write(matrix[i, j] + " ");
+            matrix[i, j] = new Random().Next(0, 11);
+            Console.Write($"{matrix[i, j]} \t");
         }
         Console.WriteLine();
     }
 }
 
-void FindArithmeticColumnMatrix(int[,] matrix)
-{
-    double arithmeticColumns = 0;
-    double sumColumns = 0;
 
-    for (int j = 0; j < matrix.GetLength(1); j++)
+void PrintMatrixAvg(int[,] matrix)
+{
+    double sumColumn = 0;
+    double avrgColumn = 0;
+    for (int j = 0; j < matrix.GetLength(0); j++)
     {
-        for (int i = 0; i < matrix.GetLength(0); i++)
+        sumColumn = 0;
+        for (int i = 0; i < matrix.GetLength(1); i++)
         {
-            sumColumns += matrix[i, j];
+            sumColumn += matrix[i, j];
+
         }
-        arithmeticColumns = sumColumns / matrix.GetLength(0);
-        Console.Write($"Сумма {j + 1} столбца = {sumColumns} ");
-        Console.WriteLine($" Среднеарифметическое = {arithmeticColumns:F2}");
-        sumColumns = 0;
+        avrgColumn = sumColumn / matrix.GetLength(0);
+        Console.Write($"Сумма {j + 1} столбца = {sumColumn} ");
+        Console.WriteLine($" Среднеарифметическое столбца = {avrgColumn:f1}");
+        /* Console.WriteLine($"{avrgColumn} \t"); */
+        sumColumn = 0;
     }
+
 }
 
-Console.WriteLine("Введите число строк");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число столбцов");
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = InputMatrix(m, n);
-PrintMatrix(matrix);
-FindArithmeticColumnMatrix(matrix);
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+
+Console.WriteLine("Массив: ");
+InputMatrix(matrix);
+/* PrintMatrix(matrix); */
+
+PrintMatrixAvg(matrix);
